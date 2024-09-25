9b2724452ae72560db2984eb65164373a2ee688b
ricardo frança soares <rickfsoaresdev@gmail.com>
2024-09-25 18:34:01 -0300

    first commit

--- src/main/java/br/edu/ifpb/tsi/pdist/cinema/DTO/Mensagem.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.tsi.pdist.cinema.DTO;
-
-import lombok.Data;
-
-@Data
-public class Mensagem {
-    private String mensagem;
-    private String key;
-}
--- src/main/java/br/edu/ifpb/tsi/pdist/cinema/service/HelloWorldService.java
+++ src/main/java/br/edu/ifpb/tsi/pdist/cinema/service/HelloWorldService.java
@@ -2,27 +2,11 @@ package br.edu.ifpb.tsi.pdist.cinema.service;
 
 import org.springframework.stereotype.Service;
 
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-
 @Service
 public class HelloWorldService {
-   private static JedisPool connection = new JedisPool("redis", 6379); 
 
    public HelloWorldService() {
 
    }
 
-   public String getMensagem(String key) {
-       try (Jedis jedis = connection.getResource()) {
-            return jedis.get(key);   
-        }
-
-   }
-
-   public void criar(String key, String mensagem) {
-        try (Jedis jedis = connection.getResource()) {
-            jedis.append(key, mensagem);
-        }       
-   }
 }
